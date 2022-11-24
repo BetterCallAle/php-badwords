5542f09898fe345a717b68eabcacb314d7823157
BetterCallAle <dap22091998@gmail.com>
2022-11-24 15:04:59 +0100

    exercise completed

--- result.php
+++ result.php
@@ -1,6 +1,8 @@
 <?php 
-$user_paragraph = $_GET["paragraph"];
-$user_bad_word = $_GET["badword"];
+    $user_paragraph = $_GET["paragraph"];
+    $user_paragraph_len = strlen($user_paragraph);
+    $user_bad_word = $_GET["badword"];
+    $user_paragraph_censored = str_replace($user_bad_word, "***", $user_paragraph)
 ?>
 
 <!DOCTYPE html>
@@ -9,9 +11,21 @@ $user_bad_word = $_GET["badword"];
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Result</title>
 </head>
 <body>
     
+    <h2>Paragrafo Originale:</h2>
+    <p><?php echo $user_paragraph ?></p>
+    <small>Lunghezza del paragrafo: <?php echo $user_paragraph_len ?></small>
+    <hr>
+
+    <h2>Paragrafo Censurato:</h2>
+    <p> <?php echo $user_paragraph_censored ?> </p>
+
+    <hr>
+
+    <a href="index.php">Scrivi un nuovo paragrafo</a>
+
 </body>
 </html>
